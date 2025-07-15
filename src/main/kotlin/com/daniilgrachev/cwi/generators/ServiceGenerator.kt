c516c5708b2d3fa18882c4ccb52b605215f9d5d4
Daniil Grachev <gggchaseggg@gmail.com>
2025-07-15 19:46:23 +0300

    feat: add service class creation

---
+++ src/main/kotlin/com/daniilgrachev/cwi/generators/ServiceGenerator.kt
@@ -0,0 +1,58 @@
+package com.daniilgrachev.cwi.generators
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiDirectory
+
+data class ServiceDialogResult(
+    override val name: String,
+    override val needExtraFolder: Boolean
+) : GeneratorsBase.DialogResult
+
+class ServiceGenerator: GeneratorsBase(dirSuffix = "Service") {
+    override fun showDialog(project: Project): DialogResult? {
+        val value = Messages.showInputDialogWithCheckBox(
+            "",
+            "Create Service Class",
+            "Create extra folder",
+            false,
+            true,
+            null,
+            "",
+            NameValidator()
+        )
+
+        val storeName = value.first
+        val needExtraFolder = value.second
+
+        if (storeName == null || needExtraFolder == null) return null
+
+        return ServiceDialogResult(storeName, needExtraFolder)
+    }
+
+    override fun runGeneration(
+        project: Project,
+        rootDir: PsiDirectory,
+        params: DialogResult,
+        capitalizedEntityName: String,
+        dir: PsiDirectory
+    ) {
+        // ---------- Файл ----------
+        val fileName = getFileName(capitalizedEntityName, "service")
+        // -------------- Контент --------------
+        val fileContent = """            
+            export class ${capitalizedEntityName}Service {
+                
+            }
+
+        """.trimIndent()
+
+        createFileWithContent(project, dir, fileName, fileContent)
+
+        // ---------- index.ts: создать или дополнить ----------
+        generateIndexFiles(project, capitalizedEntityName, dir, params)
+
+        // ---------- Открываем созданный файл в редакторе ----------
+        openFile(project, fileName, dir)
+    }
+}
